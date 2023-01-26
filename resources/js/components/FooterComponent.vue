039064e9c292737a85baa535fe811251e2de8a36
Subkhan <phonephone24122021@gmail.com>
2023-01-26 19:00:56 +0300

    FooterBriskMeal

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,6 +1,222 @@
 <template>
-    <div class="container footerBlock" >
-        Footer
+   <footer>
+     <div class="footer-mobile">
+    <a class="footer-mobile-download" href="#">Download our app</a>
+    <div class="accordion" id="accordionExample">
+    <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <svg class="svg-footer" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" color="inherit"><path fill-rule="evenodd" clip-rule="evenodd" d="m12 16.333-6-6L7.333 9 12 13.667 16.666 9 18 10.333l-6 6Z" fill="#FFF"></path></svg>
+         <strong class="strong-footer-chef" >Green Chef</strong> 
+      </button>
+      </h2>
+      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <div class="accordionmenu">
+                                   <div>
+                                        <a>
+                                  <span>Weekly Menu</span>
+                                        </a>
+                                    </div>
+                                    <div class="accordionmenu">
+                                        <a>
+                                            <span>Our Plans</span>
+                                        </a>
+                                    </div>
+                                    <div class="accordionmenu">
+                                        <a>
+                                            <span>Delivery Options</span>
+                                        </a>
+                                    </div>
+                                    <div  class="accordionmenu">
+                                        <a> 
+                                            <span>How It Works</span>
+                                        </a>
+                                    </div>
+                                    <div  class="accordionmenu">
+                                        <a>
+                                            <span>Hero Discounts</span>
+                                        </a>
+                                    </div>
+     </div> 
+    </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+         <strong class="strong-footer-chef">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;Help center</strong> 
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+         Help center and FAQ
+      </div>
+    </div>
+  </div>
+  
+</div> 
+   </div>
+
+
+
+<div class="footer-menu">
+    <div class="footer-menu-left">
+   <div class="footer-menu-greenchef">
+                         <div class="footer-bold-title"><span>Green Chef</span></div>
+                         <div class="briskunderline">
+                             <div class="accordionmenu">     
+                                        <a>
+                                  <span>Weekly Menu</span>
+                                        </a>
+                                    </div>
+                                    <div class="accordionmenu">
+                                        <a>
+                                            <span>Our Plans</span>
+                                        </a>
+                                    </div>
+                                    <div class="accordionmenu">
+                                        <a>
+                                            <span>Delivery Options</span>
+                                        </a>
+                                    </div>
+                                    <div  class="accordionmenu">
+                                        <a> 
+                                            <span>How It Works</span>
+                                        </a>
+                                    </div>
+                                    <div  class="accordionmenu">
+                                        <a>
+                                            <span>Hero Discounts</span>
+                                        </a>
+                                    </div >
+                               </div> 
+                            </div> 
+    
+
+    <div class="helpAll">
+    
+    
+        <div class="footer-bold-title"><span>Help center</span></div>
+          <div  class="helpunderline">
+                                        <a>
+                                            <span>Help center and FAQ</span>
+                                        </a>
+                                    </div >
+    </div>
+                
+</div>        
+             
+            
+                <div class="download">
+                    <div>
+                    <span><strong>Download our app</strong></span>
+                   </div> 
+                        
+                    <p class="manage" >
+                        <span>Manage your deliveries from anywhere, anytime.</span>
+                    </p>
+                    <div class="manage">
+                        <div>
+                            <a  href="https://apps.apple.com/us/app/green-chef/id1545961260">
+                                <span>
+                                    <span style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"><span style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;">
+                                        <img alt="" aria-hidden="true" src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27126%27%20height=%2742%27/%3e" style="display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;">
+                                    </span>
+                                    <img id="app-store" alt="apple-store-badge" src="https://cdn.hellofresh.com/us/cms/ER/badges/appstore-badge-en.png" decoding="async" data-nimg="intrinsic" style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;" srcset="https://cdn.hellofresh.com/us/cms/ER/badges/appstore-badge-en.png 1x, https://cdn.hellofresh.com/us/cms/ER/badges/appstore-badge-en.png 2x">
+                                    <noscript></noscript>
+                                </span>
+                            </span>
+                        </a>
+                    </div>
+                    <div  text-decoration="underline" href="https://play.google.com/store/apps/details?id=com.greenchef.GreenChef" data-zest="greenchef" class="google">
+                        <span type="body-md-regular">
+                            <span style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;">
+                                <span style="box-sizing: border-box; display: block; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; max-width: 100%;">
+                                    <img alt="" aria-hidden="true" src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%27126%27%20height=%2742%27/%3e" style="display: block; max-width: 100%; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px;">
+                                </span>
+                                <img id="play-store" alt="google-play-badge" src="https://cdn.hellofresh.com/us/cms/ER/badges/playstore-badge-en.png" decoding="async" data-nimg="intrinsic" style="position: absolute; inset: 0px; box-sizing: border-box; padding: 0px; border: none; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;" srcset="https://cdn.hellofresh.com/us/cms/ER/badges/playstore-badge-en.png 1x, https://cdn.hellofresh.com/us/cms/ER/badges/playstore-badge-en.png 2x">
+                                <noscript></noscript>
+                            </span>
+                        </span>
+                
+                </div>
+            </div>
+    
+    </div>
+</div>
+    
+   
+      <div class="footerbottomall">  
+         
+    
+                <div>
+                    <span class="greencheffooter">© 
+                        <span>Green Chef</span>&nbsp2023
+                    </span>
+                </div>
+                
+                <div class="footerbottom"> 
+
+                        <a class="footerbottom">
+                            <span>Accessibility</span>
+                        </a>
+                    
+                    
+                    
+                    <a class="footerbottom">
+                        <span>Terms &amp; Conditions</span>
+                    </a>
+                
+            
+            
+                    <a class="footerbottom">
+                        <span>Privacy Policy</span>
+                    </a>
+                
+                
+                
+                    <a class="footerbottom">
+                        <span>Do not sell my personal information</span>
+                    </a>
+                
+            
+            
+        </div>
+    
+    
+        <div class="socialmedia">
+            <div class="facebook">
+                <a target="_blank" rel="noopener noreferrer" text-decoration="underline" href="https://www.facebook.com/greenchefpage" data-zest="greenchef" class="sc-10c73c59-0 jExSfB">
+                    <span>
+                        <svg width="25" height="25" viewBox="0 0 48 48" fill="#1877F2" xmlns="http://www.w3.org/2000/svg" aria-labelledby="facebook">
+                            <title id="facebook">facebook</title>
+                            <path d="M48 24C48 10.745 37.255 0 24 0S0 10.745 0 24c0 11.98 8.776 21.908 20.25 23.708v-16.77h-6.094V24h6.094v-5.288c0-6.014 3.583-9.337 9.065-9.337 2.626 0 5.372.469 5.372.469v5.906h-3.026c-2.981 0-3.911 1.85-3.911 3.748V24h6.656l-1.064 6.938H27.75v16.77C39.224 45.908 48 35.98 48 24"></path>
+                            <path fill="#fff" d="M33.342 30.938L34.406 24H27.75v-4.502c0-1.898.93-3.748 3.911-3.748h3.026V9.844s-2.746-.469-5.372-.469c-5.482 0-9.065 3.322-9.065 9.337V24h-6.094v6.938h6.094v16.77a24.174 24.174 0 007.5 0v-16.77h5.592z" class="facebook"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+            <div class="instagram">
+                <a target="_blank" rel="noopener noreferrer" text-decoration="underline" href="https://www.instagram.com/greenchef" data-zest="greenchef" class="sc-10c73c59-0 jExSfB">
+                    <span>
+                        <svg width="25" height="25" viewBox="0 0 24 24" fill="#E1306C" xmlns="http://www.w3.org/2000/svg" aria-labelledby="instagram">
+                            <title id="instagram">instagram</title>
+                            <path d="M12 0c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 18.627 0 12 5.373 0 12 0"></path>
+                            <g class="instagram">
+                                <path fill="#fff" d="M12.242 5.25c-1.9 0-2.137.008-2.883.042-.744.034-1.252.152-1.697.325-.46.179-.85.418-1.238.807a3.426 3.426 0 00-.807 1.238c-.173.445-.291.953-.325 1.697-.034.746-.042.984-.042 2.883s.008 2.137.042 2.882c.034.745.152 1.253.325 1.698.179.46.418.85.807 1.238.388.389.778.628 1.238.806.445.173.953.291 1.697.325.746.034.984.042 2.883.042s2.137-.008 2.882-.042c.745-.034 1.253-.152 1.698-.325a3.43 3.43 0 001.238-.806c.389-.389.628-.779.806-1.238.173-.445.291-.953.325-1.698.034-.745.042-.983.042-2.882 0-1.9-.008-2.137-.042-2.883-.034-.744-.152-1.252-.325-1.697a3.428 3.428 0 00-.806-1.238 3.428 3.428 0 00-1.238-.807c-.445-.173-.953-.291-1.698-.325-.745-.034-.983-.042-2.882-.042m0 1.26c1.867 0 2.088.007 2.825.04.682.032 1.052.146 1.298.241.327.127.56.279.804.523.245.245.396.478.523.804.096.247.21.617.241 1.298.034.738.04.959.04 2.826s-.006 2.088-.04 2.825c-.031.682-.145 1.052-.24 1.298-.128.327-.28.56-.524.804a2.168 2.168 0 01-.804.523c-.246.096-.616.21-1.298.24-.737.035-.958.042-2.825.042-1.867 0-2.088-.007-2.825-.041-.682-.031-1.052-.145-1.299-.24a2.167 2.167 0 01-.804-.524 2.167 2.167 0 01-.523-.804c-.095-.246-.21-.616-.24-1.298-.034-.737-.041-.958-.041-2.825 0-1.867.007-2.088.04-2.826.032-.681.146-1.051.241-1.298.127-.326.279-.56.523-.804.245-.244.478-.396.804-.523.247-.095.617-.21 1.299-.24.737-.034.958-.041 2.825-.041"></path>
+                                <path fill="#fff" d="M12.242 14.572a2.33 2.33 0 110-4.66 2.33 2.33 0 010 4.66m0-5.92a3.59 3.59 0 100 7.18 3.59 3.59 0 000-7.18M16.813 8.51a.839.839 0 11-1.678 0 .839.839 0 011.678 0"></path>
+                            </g>
+                            <defs>
+                                <clipPath id="instagram">
+                                    <path fill="#fff" transform="translate(.69)" d="M0 0h48v48H0z"></path>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+    </div>
 </div>
 
 
